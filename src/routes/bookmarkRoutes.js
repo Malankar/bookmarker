729a48dac52e853ef22193c8d4dc18bc97e87e23
Malankar <avdhut.satish@anywhere.co>
2024-10-05 21:23:56 +0530

    revert: add rate limiting for create and delete endpoints

--- src/routes/bookmarkRoutes.js
+++ src/routes/bookmarkRoutes.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const bookmarkController = require('../controllers/bookmarkController');
-const { default: rateLimit } = require('express-rate-limit');
-
-const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	handler: (req, res) => {
-    res.status(429).json({
-      error: "Too many requests, please try again later."
-    });
-  },
-	standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-})
 
 router.get('/bookmarks', bookmarkController.getAllBookmarks);
 router.get('/bookmarks/:id', bookmarkController.getBookmarkById);
-router.post('/bookmarks', limiter, bookmarkController.createBookmark);
-router.delete('/bookmarks/:id', limiter, bookmarkController.deleteBookmarkById);
+router.post('/bookmarks', bookmarkController.createBookmark);
+router.delete('/bookmarks/:id', bookmarkController.deleteBookmarkById);
 
 module.exports = router;
