862984bd2cd390f64d90c57e5cfb16e744724c43
Malankar <avdhut.satish@anywhere.co>
2024-10-05 19:12:38 +0530

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const bookmarkRoutes = require('./routes/bookmarkRoutes');
+
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+
+app.listen(port, () => {
+  console.log(`Server listening on port http://localhost:${port}`);
+});
