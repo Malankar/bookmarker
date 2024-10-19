e2b10b2600eddd7e30732eedeadeb393c4511e49
Malankar <avdhut.satish@anywhere.co>
2024-10-19 14:24:10 +0530

    feat: add express async handler

--- src/controllers/bookmarkController.js
+++ src/controllers/bookmarkController.js
@@ -1,58 +1,43 @@
+import asyncHandler from "express-async-handler";
 import bookmarkService from "../services/bookmarkService.js";
 import { ContentTypeMismatchError } from "../utils/customError.js";
 
-const getAllBookmarks = async (req, res, next) => {
-  try {
-    const bookmarks = await bookmarkService.getAllBookmarks();
-    res.status(200).json({
-      data: bookmarks,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
+const getAllBookmarks = asyncHandler(async (req, res) => {
+  const bookmarks = await bookmarkService.getAllBookmarks();
+  res.status(200).json({
+    data: bookmarks,
+  });
+});
 
-const getBookmarkById = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const data = await bookmarkService.getBookmarkById(id);
-    if (!data) {
-      return res.status(404).json({
-        message: "Bookmark not found",
-      });
-    }
-    res.status(200).json({
-      data,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const createBookmark = async (req, res, next) => {
-  try {
-    if (req.headers["content-type"] !== "application/json") {
-      throw ContentTypeMismatchError("Content-Type must be application/json");
-    }
-    const { title, url } = req.body;
-    const { bookmark } = await bookmarkService.createBookmark(title, url);
-    return res.status(201).json({
-      data: bookmark,
-    });
-  } catch (error) {
-    next(error);
-  }
-};
-
-const deleteBookmarkById = async (req, res, next) => {
+const getBookmarkById = asyncHandler(async (req, res) => {
   const { id } = req.params;
-  try {
-    await bookmarkService.deleteBookmarkById(id);
-    return res.status(204).send();
-  } catch (error) {
-    next(error);
+  const data = await bookmarkService.getBookmarkById(id);
+  if (!data) {
+    return res.status(404).json({
+      message: "Bookmark not found",
+    });
   }
-};
+  res.status(200).json({
+    data,
+  });
+});
+
+const createBookmark = asyncHandler(async (req, res) => {
+  if (req.headers["content-type"] !== "application/json") {
+    throw ContentTypeMismatchError("Content-Type must be application/json");
+  }
+  const { title, url } = req.body;
+  const { bookmark } = await bookmarkService.createBookmark(title, url);
+  return res.status(201).json({
+    data: bookmark,
+  });
+});
+
+const deleteBookmarkById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  await bookmarkService.deleteBookmarkById(id);
+  return res.status(204).send();
+});
 
 const bookmarkController = {
   getAllBookmarks,
