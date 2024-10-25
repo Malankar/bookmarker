07189f8a08ecb262f01ca0e7674c8e61f39ff247
Malankar <avdhut.satish@anywhere.co>
2024-10-25 11:58:00 +0530

    feat: add auth middleware

--- src/server.js
+++ src/server.js
@@ -1,24 +1,42 @@
-import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import bookmarkRoutes from './routes/bookmarkRoutes.js';
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+import bookmarkRoutes from "./routes/bookmarkRoutes.js";
+import session from "express-session";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const sessionSecret = process.env.SESSION_SECRET;
+const sessionConfig = {
+  secret: sessionSecret,
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    httpOnly: true, // Prevent client-side access to cookies
+    sameSite: "strict", // Mitigate CSRF attacks
+  },
+};
+
+if (process.env.NODE_ENV === "production") {
+  sessionConfig.cookie.secure = true; // Enable only for HTTPS
+}
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(session(sessionConfig));
 
 // Serve the index.html file
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // Use the bookmark routes
-app.use('/v1', bookmarkRoutes);
+app.use("/v1", bookmarkRoutes);
 
 app.listen(port, () => {
   console.log(`Server listening on port http://localhost:${port}`);
